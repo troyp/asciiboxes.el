3355d8b79f38b9147684e4b2f0fe3392e5e89348
Troy Pracy <troyp7@gmail.com>
2017-05-19 10:48:48 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+asciiboxes.el --- Emacs interface to the boxes program
+--------------------------------------------------------------------
+
+Provides boxes [boxes][] functionality in Emacs.
+
+### Variables
+
+* `asciiboxes-comment-alist`: Default comment design by mode.
+* `asciiboxes-headings-alist`: Default comment box design by mode. If no entry
+  exists for a mode, then the design for text-mode is used and then commented
+  in a separate step.
+* `asciiboxes-boxes-command`: The boxes command used by asciiboxes.
+* `asciiboxes-config-file`: The boxes config file to be used.
+
+### Functions
+
+* `asciiboxes-list`: List all available boxes designs.
+* `asciiboxes-box-region`: Surround the region from `beg` to `end` with a box
+  of style `design`.
+* `asciiboxes-comment` Comment the region from `beg` to `end`, using style
+  `design`.
+
+
+[boxes]: http://boxes.thomasjensen.com/
---
+++ asciiboxes.el
@@ -0,0 +1,102 @@
+;;; asciiboxes.el --- Emacs interface to the boxes program
+
+;; Copyright (C) 2017 Troy Pracy
+
+;; Author: Troy Pracy
+;; Keywords: box comment
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24") ())
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 2 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Provides boxes <http://boxes.thomasjensen.com/> functionality in Emacs.
+
+;;; Code:
+
+(provide 'asciiboxes)
+
+(defvar asciiboxes-comment-alist
+  '((c-mode            . "c-cmt2")
+    (c++-mode          . "c-cmt2")
+    (java-mode         . "java-cmt")
+    (html-mode         . "html-cmt")
+    (sh-mode           . "pound-cmt")
+    (perl-mode         . "pound-cmt")
+    (python-mode       . "pound-cmt")
+    (ruby-mode         . "pound-cmt")
+    (lisp-mode         . "lisp-cmt")
+    (emacs-lisp-mode   . "lisp-cmt")
+    (scheme-lisp-mode  . "lisp-cmt")
+    (clojure-lisp-mode . "lisp-cmt")
+    (vim-mode          . "vim-cmt")
+    (ada-mode          . "ada-cmt")
+    (haskell-mode      . "ada-cmt")
+    (tex-mode          . "tex-cmt")
+    (sml-mode          . "caml-cmt")
+    (tuareg-mode       . "caml-cmt")
+    )
+  "Default comment design by mode.")
+
+(defvar asciiboxes-headings-alist
+  '((text-mode    . "simple")
+    (c-mode       . "cc")
+    (c++-mode     . "cc")
+    (java-mode    . "cc")
+    (html-mode    . "html")
+    (sh-mode      . "shell")
+    (perl-mode    . "shell")
+    (python-mode  . "shell")
+    (ruby-mode    . "shell")
+    (ada-mode     . "ada-box")
+    (haskell-mode . "ada-box")
+    (tex-mode     . "tex-box")
+    (sml-mode     . "caml")
+    (tuareg-mode  . "caml")
+    )
+  "Default comment box design by mode. If no entry exists for a mode, then the
+design for text-mode is used and then commented in a separate step.")
+
+(defvar asciiboxes-boxes-command "/usr/bin/boxes" "The boxes command used by asciiboxes.")
+(defvar asciiboxes-config-file "~/.boxes" "The boxes config file to be used.")
+
+(defun asciiboxes-list ()
+  "List all available boxes designs."
+  (s-split "\\( \\|\t\\|\n\\)+"
+           (shell-command-to-string
+    (format "cat %s | sed -nr 's/BOX (.*)/\\1/p'" asciiboxes-config-file))))
+
+(defun asciiboxes-box-region (beg end design)
+  "Surround the region from BEG to END with a box of style DESIGN."
+  (interactive
+   (list (if (region-active-p) (region-beginning) (line-beginning-position))
+         (if (region-active-p) (region-end) (line-end-position))
+         (completing-read "Design: " (asciiboxes-list))))
+   (let ((cmd (format "%s -d %s" asciiboxes-boxes-command design)))
+     (shell-command-on-region beg end cmd nil t)
+     (message cmd)))
+
+(defun asciiboxes-comment (beg end design)
+  "Comment the region from BEG to END, using style DESIGN."
+  (interactive
+   (list (if (region-active-p) (region-beginning) (line-beginning-position))
+         (if (region-active-p) (region-end) (line-end-position))
+         (or (alist-get major-mode asciiboxes-comment-alist)
+             (completing-read "Design: " (seq-uniq (mapcar #'cdr asciiboxes-comment-alist))))))
+  (shell-command-on-region beg end
+                           (format "%s -d %s" asciiboxes-boxes-command design)
+                           nil t))
+
+;;; asciiboxes ends here
